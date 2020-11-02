de84fefec37623094cd7fe413bbcf93ef996c92b
jaditya8109 <jaditya8109@gmail.com>
2020-11-03 00:40:13 +0530

    Update index.js
    web scrapped reviews saved in array and array data is passed into model via axios and model returns the output in console along with array elements

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const cheerio = require('cheerio');
 const request = require('request');
+var express = require('express');  
+var app     = express(); 
+const axios = require('axios');
+var parse = require('csv-parse');
+const fs = require('fs');
+
+// scrapping 
+
 //paste ur URL below
 const productURL = 'https://www.amazon.in/Redmi-8A-Dual-Blue-Storage/dp/B07X4R63DF/ref=psdc_1805560031_t1_B01MEGHPZ4'
 //To save in csv
-const fs = require('fs');
+
 const writeStream = fs.createWriteStream('data.csv');
 
+var dataa = [];
 request(productURL , (error, response, html) => {
   if (!error && response.statusCode == 200) {
     const $ = cheerio.load(html);
@@ -15,17 +24,59 @@ request(productURL , (error, response, html) => {
     reviews.each((i, w) => {
         // Find the review comments
         const textReview = $(w).find('.review-text').text().replace(/\s\s+/g, '');
-        // stars
-        const stars = $(w).find('.review-rating').text();
-        // name 
-        const name = $(w).find('.a-profile-name').text();
         
         //printing on console
-        console.log(textReview);
+        // console.log(textReview);
 
         // Write Row To CSV
-        writeStream.write(`${textReview}, ${stars}, ${name} \n`);
+        writeStream.write(`${textReview}\n`);
+        //pushing data in array
+        dataa.push(`${textReview}` );
     });
-    console.log('Damn!');
+    
+    console.log(dataa);
+    console.log('data saved!');
+    for(var i = 0; i<dataa.length; i++){
+      // calling model api using axios
+
+          axios.get('https://afrd.herokuapp.com/', {
+            params: {
+              query: dataa[i]
+            }
+          }).then ((data)=>console.log(data.data))
+    }
   }
 });
+
+// transfering data from csv to array
+
+// var csvData=[];
+// fs.createReadStream('data.csv')
+//     .pipe(parse({delimiter: ':'}))
+//     .on('data', function(csvrow) {
+//         // console.log(csvrow);
+//         //do something with csvrow
+//         csvData.push(csvrow);        
+//     })
+//     .on('end',function() {
+//       //do something with csvData
+//       // console.log(csvData[2][0]);
+//       for (var i = 0; i < csvData.length; i++) {
+//         // calling model api using axios
+
+//           axios.get('https://afrd.herokuapp.com/', {
+//             params: {
+//               query: csvData[i][0]
+//             }
+//           }).then ((data)=>console.log(data.data))
+//         console.log(csvData[i][0]);
+//       }
+//     });
+
+// calculation
+
+
+
+  app.listen(8000 , () => {
+    console.log('listening to port 8000');
+  });
